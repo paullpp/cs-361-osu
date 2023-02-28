51c81e08a9ec7bdf5283bcd84468a95158b79495
Paul Lipp <paullipp@10-248-87-220.wireless.oregonstate.edu>
2023-02-28 12:27:06 -0800

    removing json file

--- project/server.js
+++ project/server.js
@@ -1,7 +1,6 @@
 var fs = require('fs')
 var express = require('express')
 var exphbs = require('express-handlebars')
-var data = require('./meet_data.json')
 const bodyParser = require('body-parser');
 const { connectToDb } = require('./lib/mongo')
 const {
@@ -15,16 +14,16 @@ const {
 var app = express()
 var port = 8000
 
-var names = []
-var totals = []
-var weightclasses = []
-for (var i = 0; i < data.Lifters.length; i++) {
-  names[i] = data.Lifters[i].name
-  totals[i] = [data.Lifters[i].name, data.Lifters[i].total]
-  if (!(weightclasses.includes(data.Lifters[i].weightclass))) {
-    weightclasses.push(data.Lifters[i].weightclass)
-  }
-}
+// var names = []
+// var totals = []
+// var weightclasses = []
+// for (var i = 0; i < data.Lifters.length; i++) {
+//   names[i] = data.Lifters[i].name
+//   totals[i] = [data.Lifters[i].name, data.Lifters[i].total]
+//   if (!(weightclasses.includes(data.Lifters[i].weightclass))) {
+//     weightclasses.push(data.Lifters[i].weightclass)
+//   }
+// }
 
 app.engine('', exphbs.engine({ defaultLayout: null}))
 
@@ -44,17 +43,17 @@ app.get('/api/v1/meetdata', async function (req, res, next) {
   res.status(200).send(meets);
 })
 
-app.get('/api/v1/lifters', function (req, res, next) {
-  res.status(200).send(names)
-})
+// app.get('/api/v1/lifters', function (req, res, next) {
+//   res.status(200).send(names)
+// })
 
-app.get('/api/v1/totals', function (req, res, next) {
-  res.status(200).send(totals)
-})
+// app.get('/api/v1/totals', function (req, res, next) {
+//   res.status(200).send(totals)
+// })
 
-app.get('/api/v1/weightclasses', function (req, res, next) {
-  res.status(200).send(weightclasses)
-})
+// app.get('/api/v1/weightclasses', function (req, res, next) {
+//   res.status(200).send(weightclasses)
+// })
 
 app.post('/api/v1/addlifter', async function(req, res, next) {
   await insertNewLift(req.body['name'], req.body['weightclass'], req.body['bench_1'], req.body['bench_2'], req.body['bench_3'], req.body['squat_1'], req.body['squat_2'], req.body['squat_3'], req.body['deadlift_1'], req.body['deadlift_2'], req.body['deadlift_3'], req.body['total'])
